33b352876db2d43f122323d0e453baa55b90e707
ZainabRakiah <zainabrakiah2018@gmail.com>
2026-09-09 12:40:11 +0530

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        far = 0
+        n = len(nums)-1
+        boundary=0
+        jumps=0
+        for i in range(n):
+            if far<i+nums[i]:
+                far = i+nums[i]
+            if i == boundary:
+                jumps+=1
+                boundary = far
+            if far>n:
+                return jumps
+        return jumps
+
