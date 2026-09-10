693c7319d891673b5b4eb92fa7b7b333d9fa326b
ZainabRakiah <zainabrakiah2018@gmail.com>
2026-09-10 23:28:13 +0530

    Add: jump-game - submission-4

---
+++ Data Structures & Algorithms/jump-game/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        last_index = len(nums)-1
+        range_so_far=0
+        max_range = 0
+        for i in range(len(nums)):
+            range_so_far = i+nums[i]
+            max_range=max(range_so_far,max_range)
+            if range_so_far>=last_index:
+                return True
+            elif i == max_range:
+                return False
+            
